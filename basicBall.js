bae1234322078b6cfdc5e1ba68292a18e9fdc101
vernsworld <veronica.avola@pcc.edu>
2020-03-03 13:17:06 -0800

    handjob

--- basicBall.js
+++ basicBall.js
@@ -1,3 +1,8 @@
+let v1 = new p5.Vector(40, 50, 0);
+let v2 = new p5.Vector(40, 50, 0);
+let v3 = new p5.Vector(100, 100, -1);
+
+
 
 let madDude;
 
@@ -9,6 +14,7 @@ let howManyDudes = 10;
 function setup(){
 	createCanvas(windowWidth, windowHeight);
 	dude = loadImage('assets/dude.png')
+	hand = loadImage('assets/hand.png')
 	madDude = new Angry();
 
 	for (let i = 0; i < howManyDudes; i++){
@@ -22,10 +28,19 @@ function draw(){
 
 	madDude.display();
 	madDude.move();
-	for (let i = 0; i < howManyBalls; i++){
+	for (let i = 0; i < howManyDudes; i++){
 		dudes[i].move();
 		dudes[i].display();
 	}
+
+	let mouseVector = new p5.Vector(mouseX, mouseY);
+	//constrain the vector to maximum and minimum range give magnitude of 1
+	mouseVector.normalize();
+
+	image(dude, v1.x, v1.y, v3.x, v3.y);
+	image(dude, v2.x, v2.y, v3.x, v3.y);
+	v1.add(mouseVector);
+	//image(hand, v1.x, v1.y, v3.x, v3.y);
 
 }
 
@@ -38,7 +53,7 @@ class Angry {
 		this.yspeed = random(0,1);
 	}
 	display(){
-		image(dude, this.x, this.y);
+		image(hand, this.x, this.y);
 	}
 	move(){
 		this.x = this.x + this.xspeed;
